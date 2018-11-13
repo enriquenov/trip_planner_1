2e74a81d3945da26305e1c449fb15a0f54b0f74d
Neil Scheuermann <neilscheuermann@gmail.com>
2018-11-13 14:44:35 -0700

    Working on Markers

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log('pooping...');
+const mapboxgl = require("mapbox-gl");
+
+mapboxgl.accessToken = 'pk.eyJ1IjoibmVpbHNjaGV1ZXJtYW5uIiwiYSI6ImNqb2c4bm9zNDBjbmczcWp0YzZ6ajM5cXQifQ.DYKi3OjESw9ntZSGTBwVvA';
+
+const map = new mapboxgl.Map({
+  container: "map",
+  center: [-74.009, 40.705], // FullStack NY coordinates; alternatively, use [-87.6354, 41.8885] for Chicago
+  zoom: 12, // starting zoom
+  style: "mapbox://styles/mapbox/streets-v10" // mapbox has lots of different map styles available.
+});
+
+// module.exports = map;
